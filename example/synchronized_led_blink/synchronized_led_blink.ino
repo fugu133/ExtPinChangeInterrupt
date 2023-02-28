9b72430f8f5dfe380578ee0d1c1464267b326b82
fugu133 <fugu0220@gmail.com>
2023-02-28 22:49:05 +0900

    本体を追加

---
+++ example/synchronized_led_blink/synchronized_led_blink.ino
@@ -0,0 +1,25 @@
+#include <PinChangeInterrupt.h>
+
+#define ATTACH_PIN 10
+#define LED_PIN    13
+
+PinChangeInterrupt pcint;
+
+void setup() {
+  pcint.attachInterrupt(ATTACH_PIN, pin_change_handler);
+}
+
+void loop() {
+  
+}
+
+void pin_change_handler() {
+  volatile static bool led_flg = true;
+
+  // RISING MODE
+  if(digitalRead(ATTACH_PIN)) {
+    digitalWrite(LED_PIN, led_flg);
+    led_flg = !led_flg;
+  }
+}
+
